098f973dc639abd92d519dc04a8ff26a41badd64
sriharsha8227 <134406141+sriharsha8227@users.noreply.github.com>
2023-05-26 20:13:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿function convert() {
+    var temperature = parseFloat(document.getElementById("inputTemp").value);
+    var unit = document.getElementById("selectFrom").value;
+    var resultElement = document.getElementById("result");
+
+    if (unit === "celsius") {
+        var fahrenheit = (temperature * 9 / 5) + 32;
+        resultElement.innerHTML = temperature + "°C = " + fahrenheit + "°F";
+    } else if (unit === "fahrenheit") {
+        var celsius = (temperature - 32) * 5 / 9;
+        resultElement.innerHTML = temperature + "°F = " + celsius + "°C";
+    }
+}
